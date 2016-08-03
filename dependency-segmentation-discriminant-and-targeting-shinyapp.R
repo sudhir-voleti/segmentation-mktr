520e2727f372f1acd241d21d6309e148426c8c18
Aashish Pandey <aashishkpandey@gmail.com>
2016-08-03 16:23:48 +0530

    x3

--- dependency-segmentation-discriminant-and-targeting-shinyapp.R
+++ dependency-segmentation-discriminant-and-targeting-shinyapp.R
@@ -5,6 +5,7 @@ try(require("MASS")||install.packages("MASS"))
 try(require("devtools")||install.packages("devtools"))
 try(require("gtable")||install.packages("gtable"))
 try(require("munsell")||install.packages("munsell"))
+try(require("labeling")||install.packages("labeling"))
 library("devtools")
 try(require("ggbiplot")||install_github("ggbiplot","vqv"))
 
