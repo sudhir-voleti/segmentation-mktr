3e2fa41b7ade3de711a9d25593b425cd62e5cd3e
Sudhir Voleti <sudhir_voleti@isb.edu>
2020-12-20 13:41:22 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,13 +1,13 @@
 ####################################################
-#      Segmentation Discriminant and Targeting     #
+#      Marketing -Segmentation                     #
 ####################################################
-
+library('dplyr')
 library("shiny")
 #library("foreign")
 
 shinyUI(fluidPage(
   # Header:
- # headerPanel("Segmentation Analysis"),
+  #headerPanel("Segmentation Analysis"),
   titlePanel(title=div(img(src="logo.png",align='right'),"Segmentation Analysis")),
   # Input in sidepanel:
   sidebarPanel(
@@ -15,7 +15,7 @@ shinyUI(fluidPage(
     fileInput("file", "Upload Segmentation data (csv file with header)"),
         
     selectInput("select", "Choose Segmentation Algo", 
-                       c("K-Means","Hierarchical","Model Based"), selected = "K-Means"),
+                       c("K-Means","Hierarchical"), selected = "K-Means"),
     
     numericInput("Clust", "Number of Segments:", 3),
     
@@ -36,9 +36,9 @@ submitButton(text = "Apply Changes", icon("refresh"))
                     
                   tabPanel("Data Input Format",
                          h4(p("Data input")),
-                         p("This shiny application requires three data inputs from the user. To do so, click on the Browse (in left side-bar panel) and upload the Segmentation data input file. Similarly upload Discriminant and Classification data sets.
-                           Note that this application can read only csv file(comma delimited file), so if you don't have csv input data file, first convert your data in csv format 
-                           and then proceed. Make sure you have top row as variable names and first column as respondent id/name in csv file"
+                         p("This shiny application requires input data from the user. To do so, click on the Browse (in left side-bar panel) and upload the Segmentation data input file.
+                            Note that this application can read only csv file(comma delimited file), so if you don't have csv input data file, first convert your data in csv format 
+                            and then proceed. Make sure you have top row as variable names and first column as respondent id/name in csv file"
                            ,align="justify"),
                          img(src = "Segmentation.png", height = 180, width = 400),p('Segmentation Sample file'),
                          br(),
@@ -55,15 +55,14 @@ submitButton(text = "Apply Changes", icon("refresh"))
                 #tabPanel("Data",h3(textOutput("caption"),tableOutput("table"))),
                 
                 tabPanel("Summary - Segmentation",h3(textOutput("caption1")), h4(div(textOutput("caption2"),style = "color:Red")),
-                         plotOutput("plotpca",height = 400, width = 500),verbatimTextOutput("summary")),
-                
-                
-                # discriminat
+                           plotOutput("plotpca",height = 400, width = 500),dataTableOutput("summary")),
                 
                 tabPanel("Plot",h3("Segments Plot"), plotOutput("plot",height = 700, width = 840)),
                 tabPanel("Data Segment",br(),
                          downloadButton('downloadData4', 'Download Segmentation file (Works only in browser)'), br(),br(),
                          dataTableOutput("table"),tags$head(tags$style("tfoot {display: table-header-group;}")))
+                
+                
                 )
       ) 
     ) 
